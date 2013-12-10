f1115773be1ad0c42bff5207cba4b3d3dbe720c7
Franz Skaaning <fs@bellcom.dk>
2013-12-10 12:38:55 +0100

    file list style

--- templates/node/node.tpl.php
+++ templates/node/node.tpl.php
@@ -122,7 +122,6 @@ hide($content['report_link']);
 
   <?php if(!empty($user_picture) || $display_submitted): ?>
     <footer<?php print $footer_attributes; ?>>
-      <?php print $user_picture; ?>
       <p class="author-datetime"><?php print $submitted; ?></p>
     </footer>
   <?php endif; ?>
