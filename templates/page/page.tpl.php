94c7d4e22bdbe5d8828e11abf29ad679a7414545
Franz Skaaning <fs@bellcom.dk>
2013-12-18 08:59:51 +0100

    bulk commit

--- templates/page/page.tpl.php
+++ templates/page/page.tpl.php
@@ -156,17 +156,15 @@
             <div id="tasks">
 
               <?php if ($primary_local_tasks): ?>
-                <ul class="tabs primary clearfix"><?php print render($primary_local_tasks); ?></ul>
+                <ul class="tabs primary clearfix"><?php print render($primary_local_tasks); ?><?php if ($action_links = render($action_links)): ?><?php print $action_links; ?><?php endif; ?></ul>
               <?php endif; ?>
 
               <?php if ($secondary_local_tasks): ?>
-                <ul class="tabs secondary clearfix"><?php print render($secondary_local_tasks); ?></ul>
+                <ul class="tabs secondary clearfix"><?php print render($secondary_local_tasks); ?>
+                </ul>
               <?php endif; ?>
 
-              <?php if ($action_links = render($action_links)): ?>
-                <ul class="action-links clearfix"><?php print $action_links; ?></ul>
-              <?php endif; ?>
-
+              
             </div>
           <?php endif; ?>
 
